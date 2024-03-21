bcfb0bdfd826bf825323a8ef6abedfc4021c0767
Filipe Francisco <filipexico@gmail.com>
2024-03-21 11:34:13 +0100

    primeiro

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+const { default: gulpImagemin } = require('gulp-imagemin');
+
+function comprimeImagens() {
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'));
+}
+
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'))
+}
+
+gulp.task('default', compilaSass);
+
+function compilaSass() {
+    return gulp.src('./source/styles/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+function funcaoPadrao(callback) {
+    console.log("Executando via Gulp");
+    callback();
+}
+
+function dizOi(callback) {
+    console.log("Olá gulp");
+    callback();
+}
+
+exports.default = gulp.parallel(funcaoPadrao, dizOi);
+exports.dizOi = dizOi;
+exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss', gulp.series(compilaSass));
+}
+
+exports.javascript = comprimeJavaScript;
+exports.images = comprimeImagens
